8507adf1e16a129a2fa226aff60e6c3b12544088
Andrés Souto <kai670@gmail.com>
2020-09-15 16:15:35 +0200

    test priorityQueue

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -1,3 +1,12 @@
+import priorityQueue from 'async/priorityQueue';
+
 import '../../templates/index.scss';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
+
+const queue = priorityQueue(function(task, callback) {
+    console.log('task', task);
+    callback();
+});
+
+queue.push('task:foo', 0);
